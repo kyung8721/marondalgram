a8fa1ddbb681a31ef838d04094c9e995dd8f9150
desktop <kyungbae8721@gmail.com>
2024-09-04 21:31:54 +0900

    [배경민-집]  - marondalgram 프로젝트 생성   - instagram 클론프로젝트  - 서버 확인용 helloController 생성

---
+++ src/test/java/com/kyung/marondalgram/MarondalgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyung.marondalgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarondalgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
