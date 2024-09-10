495144ccfcc4575df3b9489e9ab6c378f98c0a40
desktop <kyungbae8721@gmail.com>
2024-09-10 16:17:19 +0900

    [배경민 - 집]  - #1  - 이메일 중복검사 API 완성  - 닉네임 중복검사 API 완성  - nickname으로 지정된 클래스 명 및 id 등등 nickName으로 일괄 변경

--- src/main/java/com/kyung/marondalgram/user/repository/UserRepository.java
+++ src/main/java/com/kyung/marondalgram/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.kyung.marondalgram.user.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.kyung.marondalgram.user.domain.User;
 
@@ -8,5 +9,9 @@ import com.kyung.marondalgram.user.domain.User;
 public interface UserRepository {
 	
 	public int addUserRepository(User user);
+	
+	public int emailDuplicateRepository(@Param("email")String email);
+	
+	public int nickNameDuplicateRepository(@Param("nickName")String nickName);
 
 }
