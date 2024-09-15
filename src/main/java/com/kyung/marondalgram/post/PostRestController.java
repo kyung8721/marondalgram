aa752ad301abee7b16057c762e838db230219a47
게이밍 노트북[경민] <.>
2024-09-15 14:06:23 +0900

    [배경민 노트북]  - 메뉴 화면구성 일부 수정   - 홈 메뉴 및 로고 클릭 시 홈으로 돌아오는 링크 설정

---
+++ src/main/java/com/kyung/marondalgram/post/PostRestController.java
@@ -0,0 +1,12 @@
+package com.kyung.marondalgram.post;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/marondalgram/post")
+public class PostRestController {
+	
+}
